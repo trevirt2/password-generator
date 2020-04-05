f4d198e6c54861c40f637677baa758394f1468c8
trevirt2 <trevirt2@yahoo.com>
2020-04-05 17:51:02 -0500

    js changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 
-//set variables
+//set variable number, lowercase, uppercase and special charaters
 let numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 let lowerCase = "abcdefghijklmnopqrstuvwxyz".split("");
 let upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
